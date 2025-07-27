a3853745dc92e8319b10c21307bde9e476f94a36
Berk Türetken <berkturetken1997@hotmail.com>
2025-07-27 14:57:22 +0300

    feat: get the pong count from the endpoint, not from the saved file

--- log_output/random_string_generator/main.py
+++ log_output/random_string_generator/main.py
@@ -3,6 +3,11 @@ import random
 import string
 import os
 import time
+import urllib.request
+
+PING_PONG_URL = os.environ.get("PING_PONG_URL", "http://ping-pong-svc:4567/pings")
+# Testing: http://localhost:8080/pings
+# Production: http://ping-pong-svc:4567/pings
 
 # For testing
 # OUTPUT_DIR = "./log_output/random_string_generator"
@@ -12,11 +17,18 @@ OUTPUT_DIR = "/usr/src/app/files"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 OUTPUT_FILE = os.path.join(OUTPUT_DIR, "output.log")
 
+def get_pong_count():
+    try:
+        with urllib.request.urlopen(PING_PONG_URL, timeout=2) as response:
+            return response.read().decode("utf-8").strip()
+    except Exception as e:
+        return "N/A"
 
 def generate_random_string():
     random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
     iso_timestamp = datetime.now().isoformat()
-    return f"{iso_timestamp}: {random_string}"
+    pong_count = get_pong_count()
+    return f"{iso_timestamp}: {random_string}\nPing / Pongs: {pong_count}"
 
 while True:
     line = generate_random_string()
