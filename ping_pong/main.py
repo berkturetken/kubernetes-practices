ab4c092540c59ec1912f6c607909293878d08ba8
Berk Türetken <berkturetken1997@hotmail.com>
2025-07-27 14:56:00 +0300

    feat: remove writing the number of pongs to a file and serve the number from a HTTP GET request

--- ping_pong/main.py
+++ ping_pong/main.py
@@ -9,22 +9,27 @@ pong = 0
 # OUTPUT_DIR = "./log_output/random_string_generator"
 
 # For production
-OUTPUT_DIR = "/usr/src/app/files"
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-OUTPUT_FILE = os.path.join(OUTPUT_DIR, "output.log")
+# OUTPUT_DIR = "/usr/src/app/files"
+# os.makedirs(OUTPUT_DIR, exist_ok=True)
+# OUTPUT_FILE = os.path.join(OUTPUT_DIR, "output.log")
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global pong
         if self.path == '/pingpong':
-            with open(OUTPUT_FILE, "a") as f:
-                f.write(f"Ping / Pongs: {pong}\n")
+            # with open(OUTPUT_FILE, "a") as f:
+            #     f.write(f"Ping / Pongs: {pong}\n")
             response = "Executed successfully."
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(response.encode("utf-8"))
             pong += 1
+        elif self.path == '/pings':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(str(pong).encode("utf-8"))
         else:
             self.send_response(404)
             self.end_headers()
