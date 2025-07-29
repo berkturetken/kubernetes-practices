9779eb8f4b75ce13874f98728e63c3e21e9003be
Berk Türetken <berkturetken1997@hotmail.com>
2025-07-29 13:07:17 +0300

    chore: remove commented code from the ping_pong application

--- ping_pong/main.py
+++ ping_pong/main.py
@@ -1,24 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
-PORT = int(os.environ.get("PORT", 9100))
 
+PORT = int(os.environ.get("PORT", 9100))
 pong = 0
 
-# For testing
-# OUTPUT_DIR = "./log_output/random_string_generator"
-
-# For production
-# OUTPUT_DIR = "/usr/src/app/files"
-# os.makedirs(OUTPUT_DIR, exist_ok=True)
-# OUTPUT_FILE = os.path.join(OUTPUT_DIR, "output.log")
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global pong
         if self.path == '/pingpong':
-            # with open(OUTPUT_FILE, "a") as f:
-            #     f.write(f"Ping / Pongs: {pong}\n")
             response = "Executed successfully."
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
