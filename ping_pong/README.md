e2d7fee0f43f2e4dd941e37fa303bf1c219f13ec
Berk Türetken <berkturetken1997@hotmail.com>
2025-08-01 15:13:22 +0300

    feat: add db connection the ping pong app and have a descriptive README

--- ping_pong/README.md
+++ ping_pong/README.md
@@ -1,2 +1,17 @@
 # Ping-pong
 
+## How to run the application locally?
+- Start the PostgreSQL server in a Docker container and run it in the background (-d flag), and set up a database: `docker run --name local-postgres -e POSTGRES_DB=ping_pong_db -e POSTGRES_USER=ping_pong_user -e POSTGRES_PASSWORD=ping_pong_pass -p 5432:5432 -d postgres:16.9`
+- You can see that Postgres is running in the background: `docker ps`
+- Run the application in a virtual environemnt to create a table:
+    - Go to the ping_pong application folder
+    - Run `source venv/bin/activate` to activate the virtual environment.
+    - Run `python3 main.py --local` to run it locally.
+- Visit http://localhost:9100/pings to see the number of pings and http://localhost:9100/pingpong to increase the number of pings.
+
+## How to run the application in a Kubernetes cluster?
+- Run `docker build -t <docker-username>/ping_pong:latest .` to build a docker image if there is a change in the source code.
+- Run `docker push <docker-username>/ping_pong:latest` to push the docker image to Docker Hub.
+- Run `k apply -f todo_app/todo_backend/manifests/deployment.yaml`. Check whether other manifests need to be changed. For example, if you need to change anything regarding the pod responsible for Postgres, you need to update the `statefulset.yaml` file.
+- Check the logs if necessary: `k logs -f <pod_name>`
+- Visit http://localhost:8081/pings to see the number of pings and http://localhost:8081/pingpong to increase the number of pings.
--- ping_pong/main.py
+++ ping_pong/main.py
@@ -1,22 +1,74 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
+import psycopg2
+from dotenv import load_dotenv
+import sys
 
+load_dotenv()
+PORT = int(os.environ.get("PORT"))
+DB_NAME = os.environ.get("POSTGRES_DB")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DB_PORT = int(os.environ.get("POSTGRES_PORT"))
 
-PORT = int(os.environ.get("PORT", 9100))
-pong = 0
+IS_LOCAL = "--local" in sys.argv
+if IS_LOCAL:
+    HOST= os.environ.get("LOCAL_POSTGRES_HOST")
+else:
+    HOST = os.environ.get("PROD_POSTGRES_HOST")
 
+def get_db_conn():
+    print("You hit the db conn function.")
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=HOST,
+        port=DB_PORT
+    )
+
+def init_db():
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS pong_counter (id SERIAL PRIMARY KEY, count INTEGER);")
+    cur.execute("SELECT count(*) FROM pong_counter;")
+    if cur.fetchone()[0] == 0:
+        cur.execute("INSERT INTO pong_counter (count) VALUES (0);")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_pong():
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT count FROM pong_counter WHERE id=1;")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return result[0] if result else 0
+
+def increment_pong():
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute("UPDATE pong_counter SET count = count + 1 WHERE id=1;")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+init_db()
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global pong
         if self.path == '/pingpong':
+            increment_pong()
             response = "Executed successfully."
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(response.encode("utf-8"))
-            pong += 1
         elif self.path == '/pings':
+            pong = get_pong()
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
@@ -27,6 +79,9 @@ class SimpleHandler(BaseHTTPRequestHandler):
             self.wfile.write(b'404 Not Found\n')
 
 if __name__ == "__main__":
-    print(f"Welcome to the Ping Pong App! Starting server on port {PORT}...")
+    if IS_LOCAL:
+        print(f"Welcome to the Ping Pong App! Starting server on port {PORT}...")
+    else:
+        print(f"Welcome to the Ping Pong App! Starting server in a Kubernetes cluster on port 8081...")
     httpd = HTTPServer(("", PORT), SimpleHandler)
     httpd.serve_forever()
